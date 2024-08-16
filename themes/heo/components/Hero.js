78caeca22d4aaa1c1a289da4142ad69eb56c8f24
nicad <2567963964@qq.com>
2024-08-16 18:15:33 +0800

    Update Hero.js

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -383,7 +383,7 @@ function TodayCard({ cRef, siteInfo }) {
               }
             />
             <div id='more' className='select-none'>
-              {locale.COMMON.TELEGRAM_CHANNEL}
+              {locale.COMMON.RECOMMEND_POSTS}
             </div>
           </div>
         </div>
